8be556434489e91a6937e3d9c39b1c515820f7b8
Douglas <douglastsp@gmail.com>
2022-09-19 23:28:51 -0300

    axios was added

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,4 @@
 <template>
-    <!-- Content -->
     <div class="px-3 py-10 md:px-10">
         <div class="w-full sm:w-1/2 lg:w-1/3 mx-auto">
             <TodoSpinner />
@@ -15,9 +14,20 @@ import TodoSpinner from '@/components/TodoSpinner.vue';
 import TodoFormAdd from '@/components/TodoFormAdd.vue';
 import TodoItems from '@/components/TodoItems.vue';
 import TodoEmpty from '@/components/TodoEmpty.vue';
+import axios from 'axios';
 
 export default {
     name: "Home",
-    components: { TodoSpinner, TodoFormAdd, TodoItems, TodoEmpty }
+    components: { TodoSpinner, TodoFormAdd, TodoItems, TodoEmpty },
+    data() {
+      return {
+        todos: []
+      }
+    },  
+    created() {
+      axios.get('http://localhost:3000/todos').then((res) => {
+        this.todos = res.data;
+      });
+    }
 }
 </script>
